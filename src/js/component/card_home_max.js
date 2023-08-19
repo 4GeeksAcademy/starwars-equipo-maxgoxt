cc98a57d214ff7fb0036a09b8feb790c39478d6a
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-19 19:21:40 +0000

    add scroll, modify HomeCard

--- src/js/component/card_home_max.js
+++ src/js/component/card_home_max.js
@@ -4,18 +4,20 @@ import imagen from "../../img/how-to.png";
 export const HomeCard = () => {
 
     return (
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card mx-3" style={{ width: "18rem", flex: '0 0 250px' }}>
             <img src={imagen} className="card-img-top" alt='imagen' />
             <div className="card-body">
-                <h5 className="card-title d-flex justify-content-start">Card title</h5>
-                <div className="text-start mb-3">
-                    <label>List:</label><span> item</span><br/>
-                    <label>List:</label><span> item</span><br/>
+                <h5 className="card-title">Card title</h5>
+                <div className="mb-3">
+                    <label>List:</label><span> item</span><br />
+                    <label>List:</label><span> item</span><br />
                     <label>List:</label><span> item</span>
                 </div>
                 <div className="d-flex justify-content-between">
-                <button type="button" className="btn btn-outline-primary">Learn more!</button>
-                <button type="button" className="btn btn-outline-warning"><i className="fa fa-heart" style={{color: "#fec004"}}></i></button>
+                    <button type="button" className="btn btn-outline-primary">Learn more!</button>
+                    {/*AGREGAR NUMERO EN EL ID DEL <input/> PARA QUE FUNCIONE BIEN*/}
+                    <input type="checkbox" className="btn-check" id="btn-check-outlined" autocomplete="off" />
+                    <label className="btn btn-outline-warning" for="btn-check-outlined"><i className="fa fa-heart" style={{ color: "#fec004" }}></i></label>
                 </div>
             </div>
         </div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { HomeCard } from "../component/card_home_max";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="container">
 		<h2 className="text-danger">Characters</h2>
-	<div className="text-center mt-5 d-flex justify-content-between">
-		<HomeCard></HomeCard>
-		<HomeCard></HomeCard>
-		<HomeCard></HomeCard>
-		<HomeCard></HomeCard>
-	</div>
+		<div className="d-flex mb-5" style={{ overflowX: 'scroll', scrollSnapType: 'x mandatory' }}>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+		</div>
+		<h2 className="text-danger">Planets</h2>
+		<div className="d-flex" style={{ overflowX: 'scroll', scrollSnapType: 'x mandatory' }}>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+			<HomeCard></HomeCard>
+		</div>
 	</div>
 );
