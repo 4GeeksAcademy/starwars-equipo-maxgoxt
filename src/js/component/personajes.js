649051eb627e924a3e5efe05a59cc18856f8d619
Juan Coronel <juanocoronell@gmail.com>
2023-08-21 17:56:28 +0000

    maxiss

--- src/js/component/personajes.js
+++ src/js/component/personajes.js
@@ -8,21 +8,24 @@ export const Personajes = props => {
   const params = useParams();
 
   return (
-    <div>
-      <div className="jumbotron">
-        <h1 className="display-4">
-          This will show the demo element: {store.demo[params.theid].title}
-        </h1>
-        <hr className="my-4" />
-        <Link to="/">
-          <span className="btn btn-primary btn-lg" role="button">
-            Back home
-          </span>
-        </Link>
+ 
+       <div className="jumbotron d-flex align-items-center">
+       <div className="jumbotron-content">    
+       <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://media.gq.com.mx/photos/5dfbaf25d811050008602c46/16:9/w_1280,c_limit/star%20wars.jpg"/>
+        </div> 
+        <div className="col-md-6">
+          <h1>This will show the demo element: {store.demo[params.theid].title}</h1>
+          <p>ihdskadiasndlkasmdñlasda</p>
+        </div>
       </div>
-      <div className="container">
+    </div>
+     
+      <div className="container-flex">
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-md-2">
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
@@ -30,13 +33,10 @@ export const Personajes = props => {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
               </div>
             </div>
           </div>
-          <div className="col-md-4">
+          <div className="col-md-2">
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
@@ -44,13 +44,10 @@ export const Personajes = props => {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
               </div>
             </div>
           </div>
-          <div className="col-md-4">
+          <div className="col-md-2">
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
@@ -58,15 +55,48 @@ export const Personajes = props => {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
+   </div>
+     
     </div>
+
   );
 };
 
