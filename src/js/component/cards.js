832b605dd5e09b3b38efcf82d10c54fb2e47cab4
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-08-21 15:52:24 +0000

    arreglos

---
+++ src/js/component/cards.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+
+const Cards=(props)=>{
+    const{store,actions}=useContext(Context)
+    return(
+        <div class="card" style={{width: "18rem"}}>
+            <img src={`https://starwars-visualguide.com/assets/img/planets/${props.id + 1}.jpg`} class="card-img-top" alt=""/>
+            <div class="card-body">
+                <h5 class="card-title">{props.planet.name}</h5>
+                <p class="card-text">Rotation Period: {props.planet.rotation_period}</p>
+                <p class="card-text">Orbital Period: {props.planet.orbital_period}</p>
+                <Link to={"/about/planet/"+ props.id}>
+                    <span className="btn btn-primary">Learn More!</span>
+                </Link>
+                <button className="btn btn-secondary" onClick={() => actions.addfavorites(props.planet.name)}>Fav </button>
+            </div>
+        </div>
+    )
+}
+export default Cards
--- src/js/views/single.js
+++ src/js/views/single.js
@@ -8,6 +8,12 @@ export const Single = props => {
 	const params = useParams();
 	return (
 		<div className="jumbotron">
+			
+			{/* const getPlanets = async () => {
+  const response = await axios.get('https://swapi.tech/api/planets');
+  return response.data.results;
+}; */}
+
 			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
 
 			<hr className="my-4" />
@@ -15,6 +21,12 @@ export const Single = props => {
 			<Link to="/">
 				<span className="btn btn-primary btn-lg" href="#" role="button">
 					Back home
+
+					{/* fetch("https://www.swapi.tech/api/planets/1")
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.error(err)) */}
+
 				</span>
 			</Link>
 		</div>
@@ -24,3 +36,11 @@ export const Single = props => {
 Single.propTypes = {
 	match: PropTypes.object
 };
+// volver  (
+	// <Columna> _ _
+	// 	<Tarjeta> _ _
+	// 		< Tarjeta . Img  variant = "top"  src = " https://raw.githubusercontent.com/NicolasArayaB/starwars-blog-reading-list/master/dist/ img/ darthVader .jpg"  width = "200"  / >
+	// 		< Tarjeta . Img  variante = "superior"  src = " ./ img/ darthvader .jpg"  ancho = "200"  / >
+	// 		< Tarjeta . Cuerpo >
+	// 			< Tarjeta . Título > { accesorios . personaje _ nombre } < / Tarjeta . Título >
+	// 			{ tienda de caracteres [ 0 ] ? (
