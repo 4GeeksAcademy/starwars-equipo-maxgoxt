b082ca3eb9e3d29ce975fe7e384a56be450e2ea1
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-08-21 14:35:45 +0000

    falta

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,11 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+
+			people: [],
+			detallepeople: [],
+		
+				
 			demo: [
 				{
 					title: "FIRST",
@@ -37,9 +42,46 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+
+			obtenerInfohome: async function() {
+				try {
+					let response = await fetch("https://www.swapi.tech/api/people");
+					let data = await response.json();
+
+				    setStore({ people: data.results });
+					
+				
+					
+				} catch (error) {
+					console.log(error);
+					
+				}
+			},
+
+			obtenerInfoPe: async function() {
+				try {
+					let response = await fetch( "https://www.swapi.tech/api/people/1" );
+					let data = await response.json();
+
+				 	setStore({ detallepeople: data.result });
+					
+					
+					
+				} catch (error) {
+					console.log(error);
+					
+				}
+			},
+
+			
+
 		}
 	};
 };
 
+
+
+
 export default getState;
+
