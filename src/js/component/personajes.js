c2a36e35869a0bd19b810f83893bea192755aa99
Juan Coronel <juanocoronell@gmail.com>
2023-08-21 15:27:42 +0000

    hola

---
+++ src/js/component/personajes.js
@@ -0,0 +1,77 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const Personajes = props => {
+  const { store } = useContext(Context);
+  const params = useParams();
+
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-4">
+          This will show the demo element: {store.demo[params.theid].title}
+        </h1>
+        <hr className="my-4" />
+        <Link to="/">
+          <span className="btn btn-primary btn-lg" role="button">
+            Back home
+          </span>
+        </Link>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Personajes.propTypes = {
+  match: PropTypes.object
+};
+
+export default Personajes;
