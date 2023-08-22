b56929554748ac17f5f3737e9f74a4a1f1777c79
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-08-22 12:26:28 +0000

    copia de seguridad

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -6,13 +6,15 @@ import { Context } from "../store/appContext";
 export const Single = props => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
+	
+
+	useEffect(()=>{
+		actions.obtenerPlanetaSingle(params.theid)
+	})
 	return (
 		<div className="jumbotron">
 			
-			{/* const getPlanets = async () => {
-  const response = await axios.get('https://swapi.tech/api/planets');
-  return response.data.results;
-}; */}
+			
 
 			<h1 className="display-4"></h1>
 
@@ -22,10 +24,7 @@ export const Single = props => {
 				<span className="btn btn-primary btn-lg" href="#" role="button">
 					Back home
 
-					{/* fetch("https://www.swapi.tech/api/planets/1")
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => console.error(err)) */}
+					
 
 				</span>
 			</Link>
@@ -36,11 +35,3 @@ export const Single = props => {
 Single.propTypes = {
 	match: PropTypes.object
 };
-// 
-	// <Columna> _ _
-	// 	<Tarjeta> _ _
-	// 		< Tarjeta . Img  variant = "top"  src = " https://raw.githubusercontent.com/NicolasArayaB/starwars-blog-reading-list/master/dist/ img/ darthVader .jpg"  width = "200"  / >
-	// 		< Tarjeta . Img  variante = "superior"  src = " ./ img/ darthvader .jpg"  ancho = "200"  / >
-	// 		< Tarjeta . Cuerpo >
-	// 			< Tarjeta . Título > { accesorios . personaje _ nombre } < / Tarjeta . Título >
-	// 			{ tienda de caracteres [ 0 ] ? (
