694fbf74727daacc1d0f79498ef5e0dc9a52b222
Juan Coronel <juanocoronell@gmail.com>
2023-08-22 14:24:36 +0000

    buenardo

--- src/js/component/personajes.js
+++ src/js/component/personajes.js
@@ -7,10 +7,12 @@ export const Personajes = props => {
   const { store, actions } = useContext(Context);
   const params = useParams();
 
+  console.log(store.detalledePersonaje);
+  console.log("holamiamor");
+
   useEffect(() => {
     actions.obtenerInfoPerSingle(params.theid)
-
-  })
+  }, [])
 
   return (
     <div className="jumbotron d-flex align-items-center">
@@ -32,10 +34,9 @@ export const Personajes = props => {
             <div className="col-md-2">
               <div className="card">
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                  <h5 className="card-title">Name</h5>
                   <p className="card-text">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
+                  {store.detallepeople.name}
                   </p>
                 </div>
               </div>
@@ -43,10 +44,9 @@ export const Personajes = props => {
             <div className="col-md-2">
               <div className="card">
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                  <h5 className="card-title">Birth Year</h5>
                   <p className="card-text">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
+                  {store.detallepeople.birth_year}
                   </p>
                 </div>
               </div>
@@ -54,10 +54,9 @@ export const Personajes = props => {
             <div className="col-md-2">
               <div className="card">
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                  <h5 className="card-title">Gender</h5>
                   <p className="card-text">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
+                  {store.detallepeople.gender}
                   </p>
                 </div>
               </div>
