c7a662ea64361f78da37684c488bae0b3061bbf7
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-08-22 13:50:25 +0000

    arreglito basico

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -2,22 +2,22 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import { Personajes }  from "../component/personajes.js"
+import { Personajes } from "../component/personajes.js"
 
 export const Single = props => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
 	
 
-	useEffect(()=>{
+	useEffect(() => {
 		actions.obtenerPlanetaSingle(params.theid)
 	})
 	return (
 		<div className="jumbotron">
 
-			{/* <h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-			
-			
+			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
+
+
 
 			<h1 className="display-4"></h1>
 
@@ -28,7 +28,7 @@ export const Single = props => {
 				<span className="btn btn-primary btn-lg" href="#" role="button">
 					Back home
 
-					
+
 
 				</span>
 			</Link>
