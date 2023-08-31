3811785f5a3972737689fc524afc6ebd4cbf9d60
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-08-31 13:34:32 +0000

    arreglos de planetas

--- src/js/views/singledePlaneta.js
+++ src/js/views/singledePlaneta.js
@@ -25,12 +25,13 @@ export const SinglePlaneta = props => {
                                 {params.theid == 0 ?
                                     <img src="https://static.wikia.nocookie.net/esstarwars/images/b/b0/Tatooine_TPM.png" /> :
 
-                                    <img src={"https://starwars-visualguide.com/assets/img/planets/" + (params.theid + 1) + ".jpg"} />}
+                                    <img alt="esto es una imagen de un planeta"
+                                    src={"https://starwars-visualguide.com/assets/img/planets/" + (params.theid + 1) + ".jpg"} />}
                             </div>
                             <br/>
                             <div className="col-md-6">
                                 <h1>{store.detalledePlaneta.name} </h1>
-
+                                <p>It is a lonnog established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                             </div>
                         </div>
                     </div>
